20b970864e08bb681535b10663d386b42a4ab65d
Vuyo-Lengisi <lvuyo1@gmail.com>
2023-06-21 21:18:19 -0700

    hi

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include "main."
+/**
+ * main - Entry point
+ * desc - _putchar emplementation
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('i');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - print_aphabet
+ *
+ * Return: Always 0.
+ */
+void print_Alphabet(void)
+{
+		char a_to_e;
+
+
+		for (a_to_e = 'a'; a_to_e <= 'e'; a_to_e++)
+			_putchar(letter);
+
+
+		_putchar('\n');
+}
