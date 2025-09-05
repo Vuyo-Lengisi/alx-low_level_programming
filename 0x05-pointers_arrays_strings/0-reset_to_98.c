b6dfe1acefacf05737c27e188ff64ab9e1c75154
Vuyo-Lengisi <lvuyo1@gmail.com>
2025-09-06 01:17:24 +0200

    update code

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 pointer to int in parameter and update ot to 98
- * Return: n
+ * reset_to_98 - pointer to int in parameter and update ot to 98
+ *@n: pointer pointing to the number to be reset
+ * Return: returns a reset pointer
  */
 void reset_to_98(int *n)
 {
